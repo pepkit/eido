2f0dbbd27b4724cdb15eb9f96601b72b0637c1b4
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-31 13:11:21 -0500

    work on unit tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import subprocess
+import pytest
 
 class TestCLI:
     def test_cli_help(self):
@@ -8,3 +9,7 @@ class TestCLI:
     def test_cli_works(self, project_file_path, schema_file_path):
         out = subprocess.check_call(['eido', '-p', project_file_path, '-s', schema_file_path])
         assert out == 0
+
+    def test_cli_exclusiveness(self, project_file_path, schema_file_path):
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_call(['eido', '-p', project_file_path, '-s', schema_file_path, '-s', 'name', '-c'])

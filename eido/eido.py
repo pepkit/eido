09e8387e032edcaeb728f9f0329439f287c2ed2c
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-05-22 15:27:10 -0400

    document func param

--- eido/eido.py
+++ eido/eido.py
@@ -266,6 +266,7 @@ def inspect_project(p, sample_names=None, max_attr=10):
 
     :param peppy.Project p: project to inspect
     :param Iterable[str] sample_names: list of samples to inspect
+    :param int max_attr: max number of sample attributes to display
     """
     if sample_names:
         samples = p.get_samples(sample_names)

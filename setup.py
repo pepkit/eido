f8098060048f98a53ecc84ad8619000583d989a4
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-08-05 18:19:20 -0400

    update py version compatibility

--- setup.py
+++ setup.py
@@ -56,8 +56,9 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
     ],
     keywords="project, metadata, bioinformatics, sequencing, ngs, workflow",

ac341f76e900e11d101c548e00f257c8e1a8f3b7
Rafal Stepien <rstepien095@gmail.com>
2022-08-31 11:10:32 -0400

    Apply bugfix

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -33,7 +33,6 @@ class TestConversionInfrastructure:
     def test_csv_filter(
         self, save_result_mock, taxprofiler_project, taxprofiler_csv_multiline_output
     ):
-
         conv_result = run_filter(
             taxprofiler_project,
             "csv",
@@ -43,3 +42,35 @@ class TestConversionInfrastructure:
 
         assert save_result_mock.called
         assert conv_result["samples"] == taxprofiler_csv_multiline_output
+
+    def test_csv_filter_handles_empty_fasta_correclty(
+        self,
+        project_pep_with_fasta_column,
+        output_pep_with_fasta_column,
+        save_result_mock,
+    ):
+        conv_result = run_filter(
+            project_pep_with_fasta_column,
+            "csv",
+            verbose=False,
+            plugin_kwargs={"paths": {"samples": "out/basic_prj.txt"}},
+        )
+
+        assert save_result_mock.called
+        assert conv_result == {"samples": output_pep_with_fasta_column}
+
+    def test_eido_csv_filter_filters_nextflow_taxprofiler_input_correctly(
+        self,
+        project_pep_nextflow_taxprofiler,
+        output_pep_nextflow_taxprofiler,
+        save_result_mock,
+    ):
+        conv_result = run_filter(
+            project_pep_nextflow_taxprofiler,
+            "csv",
+            verbose=False,
+            plugin_kwargs={"paths": {"samples": "out/basic_prj.txt"}},
+        )
+
+        assert save_result_mock.called
+        assert conv_result == {"samples": output_pep_nextflow_taxprofiler}

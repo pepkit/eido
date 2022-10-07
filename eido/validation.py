dea59d64c14eeb32b236c25d3fee90702aa2584f
Rafal Stepien <rstepien095@gmail.com>
2022-10-07 13:04:49 -0400

    Improve error message

--- eido/validation.py
+++ eido/validation.py
@@ -36,8 +36,10 @@ def _validate_object(object, schema, exclude_case=False):
     validator = Draft7Validator(schema)
     if not validator.is_valid(object):
         errors = sorted(validator.iter_errors(object), key=lambda e: e.path)
+        for error in errors:
+            print(error.message, f'''in the following location "{".".join(error.absolute_schema_path)}"''')
         raise EidoValidationError(
-            f"Validation unsuccessful. {len(errors)} errors found.", errors
+            f"Validation unsuccessful. {len(errors)} error(s) found.", errors
         )
 
 
